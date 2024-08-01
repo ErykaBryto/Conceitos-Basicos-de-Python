a0925fac4b71b14a2c1dd727a05ecaa7a629cde0
ErykaBryto <erikabcorreia2021@gmail.com>
2024-08-01 09:35:48 -0300

    Resolução dos exercícios do Desafio 01

---
+++ conceitos_iniciais.py
@@ -0,0 +1,99 @@
+
+'''
+Mensagens 
+iniciais para 
+o usuario 
+'''
+print('ola mundo')# Comentario curto
+print('ESSE É MEU PRIMEIRO CODIGO')
+
+# Tipo interito - int
+
+'''numero = int(input('Digite seu numero: '))
+print (numero)'''
+
+# Tipo com virgula - float
+'''numero = float(input('Digite seu numero float: '))
+print (numero)'''
+
+# Tipo textual - string
+'''frase = input('Digite sua frase:')
+print(frase)'''
+
+
+# Operações mmatematicas
+# soma = +
+
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+soma= numero1+numero2
+print(soma)'''
+
+# Subtração = -
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+subtrair = numero1-numero2
+print(subtrair)'''
+
+#Multiplicação = *
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+multipliacao = numero1*numero2
+print(multipliacao)'''
+
+# Divisão = /
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+divisao = numero1/numero2
+print(divisao)'''
+
+# Divisão inteira = //
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+divisao_inteira = numero1//numero2
+print(divisao_inteira)'''
+
+# Resto da divisão = %
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+resto_divisao = numero1%numero2
+print(resto_divisao)'''
+
+
+#Incremento
+'''valor = 5
+valor +=1
+print(valor)'''
+
+#Decremento
+'''valor = 5
+valor -=1
+print(valor)'''
+
+#Ordem de precedêcia
+
+'''calculo = (2+5)*((5+4)*3)
+print(calculo)'''
+
+# Operadores relacionais
+
+# == : igual
+# > : maior
+# >= : maior igual
+# < : menor
+# <= menor igual
+# != : diferente
+
+# Formatação de mensagens
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+soma= numero1+numero2
+print(f'A soma dos números é {soma}')'''
+
+'''valor = 45.00
+print(f'{valor:.2f}')'''
+
+nome = 'Erika'
+print(f'Olá, {nome}!')
+
+print('Olá {}, você tem {} anos'.format('Erika',25))
