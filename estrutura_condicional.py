5705da083fefeb97742be85c8ce3449fdde215ae
ErykaBryto <erikabcorreia2021@gmail.com>
2024-08-01 12:02:26 -0300

    Anotações sobre estruturas condicionias

---
+++ estrutura_condicional.py
@@ -0,0 +1,30 @@
+# Operadores relacionais
+
+# == : igual
+# > : maior
+# >= : maior igual
+# < : menor
+# <= menor igual
+# != : diferente
+
+#Estrutura If e Else
+
+"""numero = int(input('Digite seu número'))
+if numero > 0:
+    print('Número é positivo!')
+else:
+    print('Número é negativo!')"""
+
+
+# Estrutura While
+"""numero = -1
+while numero < 0:
+    numero = int(input('Digite seu número:')) # enquanto
+print('Número positivo inserido com sucesso!')"""
+
+# Estrutura For
+
+frutas = ['Maçã', 'Banana', 'Uva'] #declarando uma lista
+for fruta in frutas: #para cada
+    print(fruta)
+
