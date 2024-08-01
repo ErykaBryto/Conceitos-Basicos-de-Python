6c81db1701458b7bb3fa68e58fba495bad8a30b2
ErykaBryto <erikabcorreia2021@gmail.com>
2024-08-01 15:38:42 -0300

    Anotações sobre listas, tuplas e dicionários

---
+++ listas_tuplas_dic.py
@@ -0,0 +1,27 @@
+# o que são listas
+
+'''lista_frutas = [] # declarando uma lista
+lista_frutas.append('Maçã')
+lista_frutas.append('Uva')
+lista_frutas.append('Banana')
+print(lista_frutas)'''
+
+# solicitando do usuário
+'''lista_frutas = [] # declarando uma lista
+fruta = input('Digite sua fruta: ')
+lista_frutas.append(fruta)
+print(lista_frutas)'''
+
+# O que são as tuplas - tuplas são listas que não podem ter valores alterados
+
+'''tupla = ('Maçã', 'Banana', 'Uva', 'Morango')
+print(tupla)'''
+
+# O que são os dicionários - 
+# dicionario = {'chave':'valor'}
+dicionario = {}
+dicionario['maca'] = 'É uma fruta'
+dicionario['carro'] = 'É um veículo'
+dicionario['gato'] = 'É um animal'
+
+print(dicionario)
